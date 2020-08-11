42132b4e30e04c72635c45d541132c5243340f67
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-08-11 10:39:19 +0300

    Updates

---
+++ precache-manifest.c22650b23ab1a26282712047b490eebf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78742653adec89e3ef8d70339818e93e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb80e0e9d76cf159cae",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "59664826a31114d32484",
+    "url": "/static/css/main.67e0a9e0.chunk.css"
+  },
+  {
+    "revision": "ccb80e0e9d76cf159cae",
+    "url": "/static/js/2.65670418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65670418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59664826a31114d32484",
+    "url": "/static/js/main.490ae43f.chunk.js"
+  },
+  {
+    "revision": "c8396220cb1ef61ecd61",
+    "url": "/static/js/runtime-main.105e9270.js"
+  }
+]);
